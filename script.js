708857c9c17a81109d26ec7a3e7468aeb6bc8ea9
Subham <subham.sinha41@gmail.com>
2021-07-24 12:12:17 +0530

    page adjusted and contact added

---
+++ script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+}
