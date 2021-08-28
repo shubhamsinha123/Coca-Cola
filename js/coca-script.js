f4bde27db8c51c5020858a8b428fa30a978f6ebe
Subham <subham.sinha41@gmail.com>
2021-08-28 12:55:10 +0530

    resouces added

--- js/coca-script.js
+++ js/coca-script.js
@@ -15,35 +15,37 @@ var swiper = new Swiper(".swiper-container", {
     },
 });
 
+// contact popup starts
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
 }
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
+// contact popup ends
 
-var mybutton = document.getElementById("myBtn");
-window.onscroll = function () { scrollFunction() };
+// var mybutton = document.getElementById("myBtn");
+// window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-function topFunction() {
-    window.scrollTo(0, 0);
-}
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         mybutton.style.display = "block";
+//     } else {
+//         mybutton.style.display = "none";
+//     }
+// }
+// function topFunction() {
+//     window.scrollTo(0, 0);
+// }
 
-function myFunction() {
-    var x = document.getElementById("myNavigation");
-    if (x.className === "navigation") {
-        x.className += " responsive";
-    } else {
-        x.className = "navigation";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementById("myNavigation");
+//     if (x.className === "navigation") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "navigation";
+//     }
+// }
 
 // scroll to top functions
 
